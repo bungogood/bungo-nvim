7a942577d89eed0b2346b84c2cdc3999e9627310
Jonathan Good <github@bungogood.com>
2024-01-26 14:58:55 +0000

    Allowing for hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,38 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", builtin.find_files)
 vim.keymap.set("n", "<leader>pg", builtin.git_files)
 vim.keymap.set("n", "<leader>ps", builtin.live_grep)
+
+-- i want to be able to see gitignore which i cant currently see but i dont want anything in node_modules, dist, .venv venv .git etc
+
+telescope.setup({
+    defaults = {
+        hidden = true,
+        file_ignore_patterns = {
+            "node_modules/",
+            "dist/",
+            ".venv/",
+            "venv/",
+            ".git/",
+            ".DS_Store",
+            ".cache",
+            ".pytest_cache",
+            "yarn.lock",
+            "package-lock.json",
+        },
+        mappings = {
+            i = {
+                ["<esc>"] = require("telescope.actions").close,
+            },
+        },
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+        live_grep = {
+            additional_args = function(_ts)
+                return { "--hidden" }
+            end
+        },
+    }
+})
